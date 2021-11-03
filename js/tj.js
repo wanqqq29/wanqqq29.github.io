96d9908c843a560d82ee4a42f7cbaafd66a158a5
luanqt <wanqqq29@gmail.com>
2021-11-03 17:01:26 +0800

    Site updated: 2021-11-03 17:01:26

---
+++ js/tj.js
@@ -0,0 +1,9 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ba727d5704de87e82519e888b016c619";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
